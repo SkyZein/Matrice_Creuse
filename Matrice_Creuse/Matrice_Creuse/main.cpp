63ff7e8b354de2fd4dd37e265e3b91e8176c088f
SkyZein <jeanphilippe.despret@gmail.com>
2015-05-19 16:19:55 +0200

    first commit

--- Matrice_Creuse/Matrice_Creuse/main.cpp
+++ Matrice_Creuse/Matrice_Creuse/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+//#include "Matrice.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    
+    Matrix Une(15,15);
+    Matrix Deux(15,15);
+    
+    
+    Une.display();
+    
+    Deux.display("Matrice2.txt");
+    
+    for (int i = 0 ; i < 20 ; i++ )
+    {
+        cout << i << " " << Une.getValue(i,2) << endl;
+    }
+    
+    //    vector < pair < int, int > > coordinate ;
+    //    coordinate = Une.getCoordinates(1) ;
+    //    for ( int i = 0 ; i < coordinate.size() ; i++ )
+    //    {
+    //        cout << "Ligne : " << coordinate[i].second << "    Colonne : " << coordinate[i].first << endl ;
+    //    }
+    //    cout << coordinate.size() ;
+    
+    Une += Deux ;
+    
+    Une.display("MatriceAdd.txt");
+    
+    
+    
     return 0;
 }
