f5842de04b55a2d04900e9acfdea319e9a7ca91f
SkyZein <jeanphilippe.despret@gmail.com>
2015-05-19 16:16:55 +0200

    first commit

---
+++ Matrice_Creuse/Matrice_Creuse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrice_Creuse
+//
+//  Created by Despret Jean-Philippe on 19/05/2015.
+//  Copyright (c) 2015 Despret Jean-Philippe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
