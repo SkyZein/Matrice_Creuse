ffc5fe35bcdc24552768e5ca3400ecaef2933485
SkyZein <jeanphilippe.despret@gmail.com>
2015-05-19 16:21:05 +0200

    first commit

---
+++ Matrice_Creuse/Matrice_Creuse/Matrice.cpp
@@ -0,0 +1,9 @@
+//
+//  Matrice.cpp
+//  Matrice_Creuse
+//
+//  Created by Despret Jean-Philippe on 19/05/2015.
+//  Copyright (c) 2015 Despret Jean-Philippe. All rights reserved.
+//
+
+#include "Matrice.h"
---
+++ Matrice_Creuse/Matrice_Creuse/Matrice.h
@@ -0,0 +1,14 @@
+//
+//  Matrice.h
+//  Matrice_Creuse
+//
+//  Created by Despret Jean-Philippe on 19/05/2015.
+//  Copyright (c) 2015 Despret Jean-Philippe. All rights reserved.
+//
+
+#ifndef __Matrice_Creuse__Matrice__
+#define __Matrice_Creuse__Matrice__
+
+#include <stdio.h>
+
+#endif /* defined(__Matrice_Creuse__Matrice__) */
